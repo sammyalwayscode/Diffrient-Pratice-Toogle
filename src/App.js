4e840fb0f58fab2a12d80593863b175900841ef7
unknown <Olorunda Samuel>
2021-04-28 14:02:27 +0100

    Praticing Toggle functions

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Methods from "./Components/FireBaseMethods/Methods";
+// import SignsToogle from "./Components/SignInSignUpToogle/SignsToogle";
+// import PageOne from "./Components/MultiPageToggle/PageOne";
+// import Click from './Components/OnePageToogle/Click'
+
+function App() {
+  return (
+    <div>
+      {/* <Click /> */}
+      {/* <PageOne /> */}
+      {/* <SignsToogle /> */}
+      <Methods />
+    </div>
+  );
+}
+
+export default App;
